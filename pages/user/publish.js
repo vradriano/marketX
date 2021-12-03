4c19de94b0dbe4257789a24b62a4d101d21d2926
Vitor Adriano <vradriano@hotmail.com>
2021-12-03 12:29:20 -0300

    price box

--- pages/user/publish.js
+++ pages/user/publish.js
@@ -6,7 +6,11 @@ import {
   TextField,
   Typography,
   Select,
-  Button
+  Button,
+  FormControl,
+  InputLabel,
+  OutlinedInput,
+  InputAdornment
 } from '@material-ui/core'
 
 import { useDropzone } from 'react-dropzone'
@@ -218,6 +222,23 @@ const Publish = () => {
             variant="outlined"
             fullWidth 
           />
+        </Box>
+      </Container>
+
+      <Container maxWidth="md" className={classes.boxContainer}>
+        <Box className={classes.box}>
+          <Typography component="h6" variant="h6" color="textPrimary">
+            Preço
+          </Typography>
+          <br />
+          <FormControl fullWidth variant="outlined"> 
+            <InputLabel>Valor</InputLabel>
+            <OutlinedInput 
+              onChange={() => {}}
+              startAdornment={<InputAdornment position="start">R$</InputAdornment>}
+              labelWidth={40}
+            />
+          </FormControl>
         </Box>
       </Container>
 
