516a3138345694232a6cc30c86f7f5e52e501b31
Vitor Adriano <vradriano@hotmail.com>
2021-12-03 09:51:12 -0300

    create feature of upload images (front)

--- pages/user/publish.js
+++ pages/user/publish.js
@@ -1,15 +1,24 @@
-import TemplateDefault from '../../src/templates/Default'
-import { 
+import { useState } from 'react'
+import {
+  IconButton,
   Box,
   Container,
   TextField,
   Typography,
   Select,
   Button
- } from '@material-ui/core'
+} from '@material-ui/core'
+
+import { useDropzone } from 'react-dropzone'
 import { makeStyles } from '@material-ui/core/styles'
+import { DeleteForever } from '@material-ui/icons'
+
+import TemplateDefault from '../../src/templates/Default'
 
 const useStyles = makeStyles((theme) => ({
+  mask: {},
+  mainImagem: {},
+
   container: {
     padding: theme.spacing(8, 0, 6)
   },
@@ -19,12 +28,83 @@ const useStyles = makeStyles((theme) => ({
   box: {
     background: theme.palette.background.white,
     padding: theme.spacing(3),
+  },
+  thumbsContainer: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    marginTop: 15,
+  },
+  dropzone: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    textAlign: 'center',
+    padding: 10,
+    margin: '0 15px 15px 0',
+    width: 200,
+    height: 150,
+    backgroundColor: theme.palette.background.default,
+    border: '2px dashed black'
+  },
+  thumb: {
+    position: 'relative',
+    width: 200,
+    height: 150,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center center',
+    margin: '0px 15px 15px 0px',
+
+    '& $mainImagem': {
+      backgroundColor: 'blue',
+      padding: '6px 10px',
+      position: 'absolute',
+      bottom: '0',
+      left: '0',
+    },
+
+    '&:hover $mask': {
+      display: 'flex',
+    },
+
+    '& $mask': {
+      display: 'none',
+      justifyContent: 'center',
+      alignItems: 'center',
+      textAlign: 'center',
+      backgroundColor: 'rgba(0, 0, 0, 0.7)',
+      width: '100%',
+      height: '100%',
+    }
   }
 }))
 
 
 const Publish = () => {
   const classes = useStyles()
+  const [files, setFiles] = useState([])
+
+  const { getRootProps, getInputProps } = useDropzone({
+    accept: 'image/*',
+    onDrop: (acceptedFile) => {
+      const newFiles = acceptedFile.map(file => {
+        return Object.assign(file, {
+          preview: URL.createObjectURL(file)
+        })
+      })
+
+      setFiles([
+        ...files,
+        ...newFiles
+      ])
+    }
+  })
+
+  const handleRemoveFile = fileName => {
+    const newStateFiles = files.filter(file => file.name !== fileName)
+
+    setFiles(newStateFiles)
+  }
+
   return (
     <TemplateDefault>
       <Container maxWidth="sm" className={classes.container}>
@@ -87,6 +167,40 @@ const Publish = () => {
           <Typography component="div" variant="body2" color="textPrimary">
             A primeira imagem é a foto principal do seu anúncio.
           </Typography>
+          <Box className={classes.thumbsContainer}>
+            <Box className={classes.dropzone} {...getRootProps()}>
+              <input {...getInputProps()} />
+              <Typography variant="body2" color="textPrimary">
+                Clique para adicionar ou arraste a imagem para aqui
+              </Typography>
+            </Box>
+
+            {
+              files.map((file, index) => (
+                <Box
+                key={file.name}
+                className={classes.thumb} 
+                style={{ backgroundImage: `url(${file.preview})` }}
+                >
+                  {
+                    index === 0 ?
+                    <Box className={classes.mainImagem}>
+                      <Typography variant="body" color="secondary">
+                        Principal
+                      </Typography>
+                    </Box>
+                    : null
+                  }
+                  <Box className={classes.mask}>
+                    <IconButton color="secondary" onClick={() => handleRemoveFile(file.name)}>
+                      <DeleteForever  fontSize="large"/>
+                    </IconButton>
+                  </Box>
+                </Box>
+              ))
+            }
+            
+          </Box>
         </Box>
       </Container>
 
