5aab7c241df6a5dd8480051058730ddf6aad853a
Vitor Adriano <vradriano@hotmail.com>
2021-12-06 15:40:46 -0300

    category validation

--- pages/user/publish.js
+++ pages/user/publish.js
@@ -13,7 +13,9 @@ import {
   FormControl,
   InputLabel,
   OutlinedInput,
-  InputAdornment
+  InputAdornment,
+  MenuItem,
+  FormHelperText
 } from '@material-ui/core'
 
 import { useDropzone } from 'react-dropzone'
@@ -87,6 +89,9 @@ const validationSchema = yup.object().shape({
     .min(6, 'O título precisa ter no mínimo 6 caracteres.')
     .max(100, 'Título muito grande!')
     .required('Campo obrigatório'),
+
+  category: yup.string()
+    .required('Campo Obrigatório')
 })
 
 
@@ -120,7 +125,8 @@ const Publish = () => {
     <TemplateDefault>
       <Formik
       initialValues={{
-        title: ''
+        title: '',
+        category: '',
       }}
       validationSchema={validationSchema}
       onSubmit={(values) => {
@@ -167,32 +173,35 @@ const Publish = () => {
                   <Typography component="h6" variant="h6" color="textPrimary">
                     Categoria
                   </Typography>
-                  <Select
-                    native
-                    value=""
-                    fullWidth
-                    onChange={() => {}}
-                    inputProps={{
-                      name: 'age',
-                    }} 
-                  >
-                    <option value="">Selecione</option>
-                    <option value="1">Bebê e Criança</option>
-                    <option value="2">Agricultura</option>
-                    <option value="3">Moda</option>
-                    <option value="3">Carros, Motos e Barcos</option>
-                    <option value="3">Serviços</option>
-                    <option value="3">Lazer</option>
-                    <option value="3">Animais</option>
-                    <option value="3">Moveis, Casa e Jardim</option>
-                    <option value="3">Imóveis</option>
-                    <option value="3">Equipamentos e Ferramentas</option>
-                    <option value="3">Celulares e Tablets</option>
-                    <option value="3">Esporte</option>
-                    <option value="3">Tecnologia</option>
-                    <option value="3">Emprego</option>
-                    <option value="3">Outros</option>
-                  </Select>
+                  <FormControl error={errors.category} fullWidth>
+                    <Select
+                      name="category"
+                      value={values.category}
+                      fullWidth
+                      onChange={handleChange}
+                    >
+                      <MenuItem value="Bebê e Criança">Bebê e Criança</MenuItem>
+                      <MenuItem value="Agricultura">Agricultura</MenuItem>
+                      <MenuItem value="Moda">Moda</MenuItem>
+                      <MenuItem value="Carros, Motos e Barcos">Carros, Motos e Barcos</MenuItem>
+                      <MenuItem value="Serviços">Serviços</MenuItem>
+                      <MenuItem value="Lazer">Lazer</MenuItem>
+                      <MenuItem value="Animais">Animais</MenuItem>
+                      <MenuItem value="Moveis, Casa e Jardim">Moveis, Casa e Jardim</MenuItem>
+                      <MenuItem value="Imóveis">Imóveis</MenuItem>
+                      <MenuItem value="Equipamentos e Ferramentas">Equipamentos e Ferramentas</MenuItem>
+                      <MenuItem value="Celulares e Tablets">Celulares e Tablets</MenuItem>
+                      <MenuItem value="Esporte">Esporte</MenuItem>
+                      <MenuItem value="Tecnologia">Tecnologia</MenuItem>
+                      <MenuItem value="Emprego">Emprego</MenuItem>
+                      <MenuItem value="Outros">Outros</MenuItem>
+                    </Select>
+                    <FormHelperText>
+                      {
+                        errors.category
+                      }
+                    </FormHelperText>
+                  </FormControl>
                   </Box>
                 </Container>
 
