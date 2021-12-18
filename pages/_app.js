6a30b9ce98eea208e4b823623b46b8ef847be2a8
Vitor Adriano <vradriano@hotmail.com>
2021-12-18 13:16:33 -0300

    refactor with next connect

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types';
 import Head from 'next/head';
 import { ThemeProvider } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
+import { ToastyProvider } from '../src/contexts/Toasty';
 import theme from '../src/theme';
 
 export default function MyApp(props) {
@@ -15,10 +16,11 @@ export default function MyApp(props) {
         <title>marketX</title>
         <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
       </Head>
-      <ThemeProvider theme={theme}>
-        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-        <CssBaseline />
-        <Component {...pageProps} />
+      <ThemeProvider theme={theme}>      
+          <ToastyProvider>
+            <CssBaseline />
+            <Component {...pageProps} />    
+          </ToastyProvider> 
       </ThemeProvider>
     </React.Fragment>
   );
